716304f415b548370f4aa7889a4c8f1c40532d4d
nswarr <nswarr@glgroup.com>
2015-03-28 03:00:48 -0400

    Yeehaw, got this socket chatting.  At all.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
+var expressWs = require('express-ws')(app)
 
 app.use(express.static('.'));
+
+app.ws('/echo', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log("Got message", msg);
+    ws.send(msg);
+  });
+});
+
 
 app.listen(process.env.PORT || 3000);
